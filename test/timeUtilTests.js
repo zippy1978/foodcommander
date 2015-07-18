7af25b01fdee949b927bb35ca8ff29af5d34a4d9
Gilles Grousset <gilles.grousset@backelite.com>
2015-07-18 20:45:43 +0200

    New estimated delivery time rounding method

--- test/timeUtilTests.js
+++ test/timeUtilTests.js
@@ -5,21 +5,32 @@ var timeUtil = require('../lib/timeUtil.js');
 
 describe('timeUtil module', function () {
 
-    it('round to next ten minutes', function () {
+    it('round to next five minutes #1', function () {
         
         var date = moment('2008-10-24 18:17:00').toDate();
         
-        var roundedDate = timeUtil.roundToNextTenMinutes(date);
+        var roundedDate = timeUtil.roundToNextFiveMinutes(date);
         
         assert.equal(20, roundedDate.getMinutes());
+        assert.equal(18, roundedDate.getHours());
     });
     
-    it('round to next ten minutes when changing hour', function () {
+    it('round to next five minutes #2', function () {
+        
+        var date = moment('2015-07-18 21:03:25').toDate();
+        
+        var roundedDate = timeUtil.roundToNextFiveMinutes(date);
+        
+        
+        assert.equal(5, roundedDate.getMinutes());
+        assert.equal(21, roundedDate.getHours());
+    });
+    
+    it('round to next five minutes when changing hour', function () {
         
         var date = moment('2008-10-24 18:56:00').toDate();
         
-        var roundedDate = timeUtil.roundToNextTenMinutes(date);
-        
+        var roundedDate = timeUtil.roundToNextFiveMinutes(date);
         
         assert.equal(0, roundedDate.getMinutes());
         assert.equal(19, roundedDate.getHours());
